6e67cda53872c93040181de1bfc2daae8c4bcf01
Hadeer Hendam <hendamhadeer9@gmail.com>
2026-09-12 17:40:40 +0300

    fix proxy redirect url

--- src/proxy.ts
+++ src/proxy.ts
@@ -9,7 +9,7 @@ export async function proxy(req: NextRequest) {
     return NextResponse.next();
   }
 
-  return NextResponse.redirect(new URL("/logIn","http://localhost:3000/logIn"));
+  return NextResponse.redirect(new URL("/logIn",req.url));
 }
 
 export const config = {
